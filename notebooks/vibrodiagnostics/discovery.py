911d4dd44d79103a99cd420b1f06ffd8a2d48bb5
Miroslav Hájek <xhajekm@stuba.sk>
2023-11-12 19:33:13 +0100

    Remove exports and feature selection rework

---
+++ notebooks/vibrodiagnostics/discovery.py
@@ -0,0 +1,478 @@
+from zipfile import ZipFile
+import numpy as np
+import pandas as pd
+from itertools import pairwise
+from typing import List
+
+from numpy.lib.stride_tricks import sliding_window_view
+from scipy.stats import skew, kurtosis, entropy
+from scipy.signal import welch, windows, find_peaks
+from scipy.interpolate import interp1d
+from scipy.fft import rfft
+
+from scipy.signal import butter, iirfilter, freqz, lfilter, decimate
+import pywt
+import tsfel
+
+import matplotlib.pylab as plt
+from vibrodiagnostics import mafaulda
+
+
+def split_dataframe(dataframe: pd.DataFrame, parts: int) -> List[pd.DataFrame]:
+    step = len(dataframe) // parts
+    return [
+        dataframe.iloc[i:i + step].reset_index(drop=True)
+        for i in range(0, len(dataframe), step)
+    ]
+
+
+def features_list():
+    config = tsfel.get_features_by_domain()
+    for domain in config.values():
+        for feature, options in domain.items():
+            if options['n_features'] != 1:
+                options['use'] = 'no'
+    return config
+
+
+def tsfel_features_generate(dataset: ZipFile, filename: str, parts=None) -> pd.DataFrame:
+    print(f'Processing: {filename}')
+
+    conf_extraction = features_list()
+    columns = mafaulda.COLUMNS
+
+    ts = mafaulda.csv_import(dataset, filename)
+    fault, severity, seq = mafaulda.parse_filename(filename)
+    dataframe = [ts] if parts is None else discovery.split_dataframe(ts, parts)
+
+    results = []
+    for i, df in enumerate(dataframe):
+        fv = pd.DataFrame({
+            'fault': [fault],
+            'severity': [severity],
+            'seq': [f'{seq}.part.{i}'],
+            'rpm': [df['rpm'].mean()]
+        })
+
+        for col in columns:
+            features = tsfel.time_series_features_extractor(
+                conf_extraction, df[col], fs=mafaulda.FS_HZ
+            )
+            features.columns = [
+                col + '_' + c.strip('0_').replace(' ', '_').lower()
+                for c in features.columns
+            ]
+            fv = fv.assign(**features)
+        results.append(fv)
+
+    return pd.concat(results).reset_index(drop=True)
+
+
+def features_time_domain(zip_file: ZipFile, filename: str, parts=None) -> pd.DataFrame:
+    print(f'Processing: {filename}')
+
+    columns = mafaulda.COLUMNS
+    ts = mafaulda.csv_import(zip_file, filename)
+    fault, severity, seq = mafaulda.parse_filename(filename)
+
+    if parts is not None:
+        dataframe = split_dataframe(ts, parts)
+    else:
+        dataframe = [ts]
+
+    result = []
+    for i, df in enumerate(dataframe):
+        fvector = [
+            ('fault', [fault]),
+            ('severity', [severity]),
+            ('seq', [f'{seq}.part.{i}']),
+            ('rpm', [df['rpm'].mean()])
+        ]
+        for col in columns:
+            x = df[col]
+            features = [
+                ('mean', [np.mean(x)]),
+                ('std', [np.std(x)]),
+                ('skew', [skew(x)]),
+                ('kurt', [skew(x)]),
+                ('rms', [mafaulda.rms(x)]),
+                ('pp', [np.max(x) - np.min(x)]),
+                ('crest', [np.max(np.absolute(x)) / mafaulda.rms(x)]),
+                ('margin', [np.max(np.absolute(x)) / (np.mean(np.sqrt(np.absolute(x))) ** 2)]),
+                ('shape', [mafaulda.rms(x) / np.mean(np.absolute(x))]),
+                ('max', [np.max(x)])
+            ]
+            features = [(f'{col}_{f[0]}', f[1]) for f in features]
+            fvector.extend(features)
+
+        result.append(pd.DataFrame(dict(fvector))) 
+
+
+    return pd.concat(result).reset_index(drop=True)
+
+
+def features_frequency_domain(zip_file: ZipFile, filename: str, parts=None) -> pd.DataFrame:
+    # Calculate FFT with Welch method in 5 different Hann window sizes
+    print(f'Processing: {filename}')
+    OVERLAP = 0.5
+    WINDOW_SIZES = (2**6, 2**8, 2**10, 2**12, 2**14)
+
+    columns = mafaulda.COLUMNS
+    ts = mafaulda.csv_import(zip_file, filename)
+    fault, severity, seq = mafaulda.parse_filename(filename)
+
+    if parts is not None:
+        dataframe = split_dataframe(ts, parts)
+    else:
+        dataframe = [ts]
+
+    result = []
+    for i, df in enumerate(dataframe):
+        fvector = [
+            ('fault', [fault]),
+            ('severity', [severity]),
+            ('seq', [f'{seq}.part.{i}']),
+            ('rpm', [df['rpm'].mean()])
+        ]
+        for window in WINDOW_SIZES:
+            for col in columns:
+                f, pxx = spectral_transform(df, col, window)
+    
+                fluxes = temporal_variation(df, col, window)
+                envelope_spectrum = envelope_signal(f, pxx)
+                loc_harmonics, _ = find_harmonics(f, pxx)
+                # inharmonicity(loc_harmonics, f, pxx)
+                # hdev(envelope_spectrum, loc_harmonics, pxx)
+        
+                features = [
+                    ('centroid', [np.average(f, weights=pxx)]),
+                    ('std', [np.std(pxx)]),
+                    ('skew', [skew(pxx)]),
+                    ('kurt', [kurtosis(pxx)]),
+                    ('roll_off', [spectral_roll_off_frequency(f, pxx, 0.85)]),
+                    ('flux_mean', [np.mean(fluxes)]),
+                    ('flux_std', [np.std(fluxes)]),
+                    ('noisiness', [signal_to_noise(pxx)]),
+                    ('energy', [energy(pxx)]),
+                    ('entropy', [entropy(pxx / np.sum(pxx))]),
+                    ('negentropy', [negentropy(envelope_spectrum)])
+                ]
+                features = [(f'{col}_{f[0]}_{window}', f[1]) for f in features]
+                fvector.extend(features)
+        result.append(pd.DataFrame(dict(fvector))) 
+
+    return pd.concat(result).reset_index(drop=True)
+
+
+def tsfel_features_import(zip_file: ZipFile, filename: str, parts=None) -> pd.DataFrame:
+    ts = mafaulda.csv_import(zip_file, filename)
+    cfg_file = tsfel.get_features_by_domain()
+
+    if parts is not None:
+        dataframe = split_dataframe(ts, parts)
+    else:
+        dataframe = [ts]
+        
+    result = []
+    for i, df in enumerate(dataframe):
+        df_result = tsfel.time_series_features_extractor(cfg_file, df[mafaulda.COLUMNS], fs=mafaulda.FS_HZ)
+        result.append(df_result)
+
+    return pd.concat(result).reset_index(drop=True)
+
+
+def features_wavelet_domain(zip_file: ZipFile, filename: str) -> pd.DataFrame:
+    print(f'Processing: {filename}')
+
+    max_level = 6
+    wavelet = 'dmey'
+    ordering = 'freq'
+
+    columns = mafaulda.COLUMNS
+    ts = mafaulda.csv_import(zip_file, filename)
+    fault, severity, seq = mafaulda.parse_filename(filename)
+
+    rpm = ts['rpm'].mean()
+    result = []
+
+    for col in columns:
+        wp = pywt.WaveletPacket(data=ts[col], wavelet=wavelet, mode='symmetric')
+
+        for feature in ('energy', 'energy_ratio', 'kurtosis', 'negentropy'):
+            wpd_header = []
+            row = {
+                'fault': fault,
+                'severity': severity,
+                'seq': seq,
+                'rpm': rpm,
+                'axis': col,
+                'feature': feature
+            }
+
+            feature_vector = []
+            for level in range(1, max_level + 1):
+                nodes = wp.get_level(level, ordering)
+
+                if feature == 'energy':
+                    e = [energy(node.data) for node in nodes]
+                    feature_vector.extend(e)
+
+                elif feature == 'energy_ratio':
+                    e = [energy(node.data) for node in nodes]
+                    total_energy = np.sum(e)
+                    energy_ratios = [energy(node.data) / total_energy for node in nodes]
+                    feature_vector.extend(energy_ratios)
+
+                elif feature == 'kurtosis':
+                    kurts = [kurtosis(node.data) for node in nodes]
+                    feature_vector.extend(kurts)
+
+                elif feature == 'negentropy':
+                    negentropies = [negentropy(node.data) for node in nodes]
+                    feature_vector.extend(negentropies)
+
+                wpd_header.extend([f'L{level}_{i}' for i in range(len(nodes))])
+
+            row.update(dict(zip(wpd_header, feature_vector)))
+            result.append(row)
+
+    return pd.DataFrame(result).reset_index(drop=True)
+
+
+###############################################################
+# https://dsp.stackexchange.com/questions/19084/applying-filter-in-scipy-signal-use-lfilter-or-filtfilt
+
+def dc_blocker(x: np.array, cutoff: float, order=1, fs=mafaulda.FS_HZ, plot=False):
+    b, a = iirfilter(order, cutoff, btype='highpass', fs=fs)
+    if plot:
+        plot_filter_response(b, a)
+    y = lfilter(b, a, x)
+    return y
+
+
+def downsample(x: np.array, k=None, fs_reduced=mafaulda.FS_HZ, fs=mafaulda.FS_HZ):
+    if k is None:
+        k = fs // fs_reduced
+    return decimate(x, k, ftype='iir')
+
+
+def lowpass_filter(x: np.array, cutoff: float, order=2, fs=mafaulda.FS_HZ, plot=False):
+    b, a = butter(order, cutoff, btype='lowpass', fs=fs)
+    if plot:
+        plot_filter_response(b, a)
+    y = lfilter(b, a, x)
+    return y
+
+
+def plot_filter_response(b, a, title=''):
+    w, h = freqz(b, a, fs=mafaulda.FS_HZ)
+    fig, ax = plt.subplots()
+    ax.plot(w, 20 * np.log10(abs(h)), 'b')
+    ax.set_xlabel('Frequency [Hz]')
+    ax.set_ylabel('Amplitude [dB]')
+    ax.set_title(title)
+
+
+###############################################################
+
+def mms_peak_finder(x: np.array, win_len=3) -> np.array:
+    a = sliding_window_view(x, window_shape=win_len)
+
+    mms_max = (
+        (np.max(a, axis=1) - np.min(a, axis=1)) /
+        (np.sum(a, axis=1) - np.min(a, axis=1) * win_len)
+    )
+    mms_mid = (
+        (a[:,win_len//2] - np.min(a, axis=1)) /
+        (np.sum(a, axis=1) - np.min(a, axis=1) * win_len)
+    )
+    peaks_in_windows, *other = np.where(mms_max == mms_mid)
+    return peaks_in_windows + 1
+
+
+def have_intersection(interval1, interval2):
+    new_min = max(interval1[0], interval2[0])
+    new_max = min(interval1[1], interval2[1])
+    return new_min <= new_max
+
+
+def harmonic_series_detection(f: np.array, Pxx: np.array, fs: int, fft_window: int) -> np.array:
+    peaks = mms_peak_finder(Pxx)
+    f_central = f[peaks]
+    delta_f = fs / fft_window
+    amplitudes = Pxx[peaks]
+
+    largest_frequency = int(fs / 2 + delta_f)
+    components = np.vstack((f_central, amplitudes)).T
+    k = 8     # Limit harmonics not detected in series (skips)
+
+    result = []
+    # Each component can be fundamental frequency
+    for v, a in components:
+        series = [(v, a)]
+
+        # Harmonic components from given fundamental frequency
+        order_distance = 0
+        delta_v = delta_f
+        for r, vi in enumerate(range(2*int(v), largest_frequency, int(v)), start=2):
+            # Search interval for harmonic component candidates
+            search_interval = (vi - ((r * delta_v) / 2), vi + ((r * delta_v) / 2))
+
+            # If tolerances are in search interval find minimal distance
+            # between true harmonic and candidate
+            candidates = []
+            for vj, aj in components:
+                tolerance_interval = (vj - delta_v / 2, vj + delta_v / 2)
+                if have_intersection(search_interval, tolerance_interval):
+                    candidates.append((vj, aj, abs(vj - vi)))
+
+            if len(candidates) > 0:
+                vh, ah, dh = min(candidates, key=lambda x: x[2])
+                series.append((vh, ah))
+                xh, yh = (vh - delta_v), (vh + delta_v)
+                # Update parameters to prevent of search interval growth
+                vi = (xh + yh) / (2*r)
+                delta_v = abs(xh - yh) / r
+
+            if order_distance == k:
+                break
+
+        if len(series) > 1:
+            result.append(series)
+
+    return result
+
+
+#################################################################
+
+def find_harmonics(f: np.array, Pxx: np.array) -> (np.array, np.array):
+    threshold = Pxx.mean() +  2*np.std(Pxx)
+    peaks, _ = find_peaks(Pxx)
+    f_harmonics = f[peaks]
+    y_harmonics = Pxx[peaks]
+
+    cond = y_harmonics >= threshold
+    loc_harmonics = peaks[cond]
+    f_harmonics = f_harmonics[cond]
+    return loc_harmonics, f_harmonics
+
+
+def envelope_signal(f: np.array, Pxx: np.array) -> np.array:
+    peaks, _ = find_peaks(Pxx)
+    # peaks = mms_peak_finder(Pxx)
+    try:
+        envelope = interp1d(f[peaks], Pxx[peaks], kind='quadratic', fill_value='extrapolate')
+    except ValueError:
+        return []
+    y_env = envelope(f)
+    y_env[y_env < 0] = 0
+    return y_env
+
+
+def spectral_transform(dataset: pd.DataFrame, axis: str, window: int) -> (np.array, np.array):
+    OVERLAP = 0.5
+    STEP = int(window * OVERLAP)
+    v = dataset[axis].to_numpy()
+    f, Pxx = welch(
+        v, fs=mafaulda.FS_HZ, window='hann',
+        nperseg=window, noverlap=STEP,
+        scaling='spectrum', average='mean', detrend='constant',
+        return_onesided=True
+    )
+    return f, Pxx
+
+###################################################################x
+
+def energy(Pxx: np.array) -> float:
+    return np.sum(Pxx**2)
+
+
+def negentropy(x: np.array) -> float:
+    if len(x) == 0:
+        return np.nan
+    return -entropy((x ** 2) / np.mean(x ** 2))
+
+
+def hdev(envelope_spectrum: np.array, loc_harmonics: np.array, Pxx: np.array) -> float:
+    if len(envelope_spectrum) == 0 or len(envelope_spectrum) == 0:
+        return np.nan
+    return np.mean(envelope_spectrum[loc_harmonics] - Pxx[loc_harmonics])
+
+
+def signal_to_noise(x: np.array) -> float:
+    # https://dsp.stackexchange.com/questions/76291/how-to-extract-noise-from-a-signal-in-order-to-get-both-noise-power-and-signal-p
+    # https://www.geeksforgeeks.org/signal-to-noise-ratio-formula/
+    # https://saturncloud.io/blog/calculating-signaltonoise-ratio-in-python-with-scipy-v11/
+    m = np.mean(x)
+    sd = np.std(x)
+    return np.where(sd == 0, 0, m / sd)
+
+
+def harmonic_product_spectrum(f: np.array, Pxx: np.array, max_harmonic=5) -> float:
+    # Returns fundamental frequency = pitch
+    # http://musicweb.ucsd.edu/~trsmyth/analysis/Harmonic_Product_Spectrum.html
+    hps_spectrum = np.copy(Pxx)
+    # Downsampling in factor of 2 .. h
+    for harmonic in range(2, max_harmonic + 1):
+        hps_spectrum[:len(Pxx[::harmonic])] *= Pxx[::harmonic]
+
+    fundamental = np.argmax(hps_spectrum)
+    return fundamental, f[fundamental]
+
+
+def inharmonicity(loc_harmonics: np.array, f: np.array, Pxx: np.array) -> float:
+    f0_pos, f0 = harmonic_product_spectrum(f, Pxx)
+    if f0 == 0 or len(loc_harmonics) == 0 or np.sum(Pxx[loc_harmonics]) == 0:
+        return np.nan
+
+    harmonic_series = np.arange(f0, f0 * (len(loc_harmonics) + 1), f0)
+
+    return (2 / f0) * np.average(np.absolute(f[loc_harmonics] - harmonic_series), weights=Pxx[loc_harmonics])
+
+
+def spectral_roll_off_frequency(f: np.array, Pxx: np.array, percentage: float) -> float:
+    # Roll-off: Cumulative sum of energy in spectral bins and find index in f array
+    # 85% of total energy below this frequency
+    return f[np.argmax(np.cumsum(Pxx**2) >= percentage * energy(Pxx))]
+
+
+def temporal_variation(dataset: pd.DataFrame, axis: str, window: int) -> list:
+    # Temporal variation of succesive spectra (stationarity)
+    OVERLAP = 0.5
+    STEP = int(window * OVERLAP)
+    v = dataset[axis].to_numpy()
+    spectra = [
+        np.absolute(rfft(v[i:i+window] * windows.hann(window)))
+        for i in range(0, len(v) - window, STEP)
+    ]
+    # f = [i * (mafaulda.FS_HZ / window) for i in range(window // 2 + 1)]
+    fluxes = [
+        1 - np.corrcoef(psd1, psd2) for psd1, psd2 in pairwise(spectra)
+    ]
+    return fluxes
+
+###############################################################################
+def plot_spectral_envelope(dataset, file, axis):
+    ts = mafaulda.csv_import(dataset, file)
+    f, Pxx = spectral_transform(ts, axis, 1024)
+    y_env = envelope_signal(f, Pxx)
+    # print(find_harmonics(f, Pxx))
+
+    fig, ax = plt.subplots(1, 1, figsize=(20, 5))
+    ax.plot(f, Pxx)
+    #ax.scatter(f[peaks], Pxx[peaks], color='red')
+    ax.plot(f, y_env)
+
+
+def plot_frequency_spectrum(dataset, file, axis, p, window=1024, dB=False, label=None):
+    ts = mafaulda.csv_import(dataset, file)
+    f, pxx = spectral_transform(ts, axis, window)
+    p.set_xlabel('Frequency [Hz]')
+    if dB:
+        p.set_ylabel('Amplitude [dB]')
+        pxx = 20 * np.log10(pxx / mafaulda.DB_REF)
+    else:
+        p.set_ylabel('Amplitude [m/s^2]')
+    p.plot(f, pxx, label=label)
+    p.grid(True)
+
---
+++ notebooks/vibrodiagnostics/mafaulda.py
@@ -0,0 +1,123 @@
+import numpy as np
+import pandas as pd
+import seaborn as sb
+
+from scipy.stats import skew, kurtosis, kstest
+from scipy.fft import rfft
+from scipy.signal import butter, filtfilt, find_peaks
+
+from tqdm.notebook import tqdm
+import matplotlib.pyplot as plt
+from zipfile import ZipFile
+
+from multiprocessing.pool import ThreadPool
+
+FS_HZ = 50000
+DB_REF = 0.000001                                # 1 dB = 1 um/s^2
+COLUMNS = ['ax', 'ay', 'az', 'bx', 'by', 'bz']   # 'mic'
+
+################################# IMPORT ########################################
+
+def parse_filename(filename: str) -> (str, str, str):
+    path = filename.split('/')
+    if path[0].strip() in ('overhang', 'underhang'):
+        fault = f'{path[0]}-{path[1]}'
+        severity = path[2]
+        seq = path[3]
+    elif path[0].strip() == 'normal':
+        fault, severity, seq = path[0], '0', path[1]
+    else:
+        fault, severity, seq = path
+
+    return fault, severity, seq
+
+
+def import_files_split(zip_file, file_list, func, parts, cores=4):
+    pool = ThreadPool(processes=cores)
+    
+    return pd.concat([
+        pool.apply_async(func, (zip_file, name, parts )).get()
+        for name in tqdm(file_list)
+    ])
+
+
+def import_files(zip_file, file_list, func, cores=4):
+    pool = ThreadPool(processes=cores)
+    
+    return pd.concat([
+        pool.apply_async(func, (zip_file, name, )).get()
+        for name in tqdm(file_list)
+    ])
+
+
+def get_mafaulda_files(zip_file):
+    filenames = [
+        text_file.filename
+        for text_file in zip_file.infolist()
+        if text_file.filename.endswith('.csv')
+    ]
+    filenames.sort()
+    return filenames
+
+
+def rpm_calc(tachometer: pd.Series) -> float:
+    t = tachometer.index.to_numpy()
+    y = tachometer.to_numpy()
+    peaks, _ = find_peaks(y, prominence=3, width=50)
+    interval = np.diff(t[peaks]).mean()
+    return 60 / interval
+
+
+def preprocess(ts: pd.DataFrame):
+    return (
+        ts
+        .assign(t = lambda x: x.index * (1 / FS_HZ))
+        .assign(mag_a = lambda x: np.hypot(x.ax, x.ay, x.ay))
+        .assign(mag_b = lambda x: np.hypot(x.bx, x.by, x.by))
+        .reset_index()
+        .assign(t = lambda x: x.index * (1 / FS_HZ))
+        .set_index('t')
+        .assign(rpm = lambda x: rpm_calc(x.tachometer))
+    )
+
+
+def csv_import(zip_file, filename):
+    ts = pd.read_csv(
+        zip_file.open(filename),
+        names=['tachometer', 'ax', 'ay', 'az', 'bx', 'by', 'bz', 'mic']
+    )
+    ts = preprocess(ts)
+    return ts.assign(key=filename)
+
+################################# DATASET INDEX ########################################
+
+def extract_metadata(zip_file: ZipFile, filename: str) -> pd.DataFrame:
+    ts = csv_import(zip_file, filename)
+    fault, severity, seq = parse_filename(filename)
+    result = [{
+        'filename': filename,
+        'fault': fault,
+        'severity': severity,
+        'seq': seq,
+        'length': len(ts.index),
+        'duration': ts.index.max(), 
+        'rpm': ts['rpm'].mean(),
+        'fs': FS_HZ,
+    }]
+    return pd.DataFrame(result)
+
+def dataset_index(path: str) -> pd.DataFrame:
+    dataset = ZipFile(path)
+    files = get_mafaulda_files(dataset)
+    return import_files(dataset, files, extract_metadata)
+
+
+def resolution_calc(fs, window):
+    print('Window size:', window)
+    print('Heinsenberg box')
+    print('\tTime step:', window / fs * 1000, 'ms')
+    print('\tFrequency step:', fs / window, 'Hz')
+
+
+def rms(x):
+    return np.sqrt(np.mean(x ** 2))
